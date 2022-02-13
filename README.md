1b2aa7670ee42a8ac9d3c9b49038cda05211b949
hsdaniel86 <hsdaniel1986@gmail.com>
2022-02-13 17:48:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Project created in the BootCamp Inter FrontEnd developer.
+### Digital Innovation One- DIO
+
+this project is based on NetFlix, created a game streaming project.
---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav: true,
+  lazyLoad: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav: true,
+  lazyLoad: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:1
+      }
+  }
+})
